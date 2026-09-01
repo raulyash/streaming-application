c2d7bec457fb64e26d12f3eb9067aeb2fd3b8473
yashraul0011 <yraul72@gmail.com>
2026-09-01 20:16:27 +0530

    Added S3Client configuration and Kafka configuration. Also, steps to publish video uploaded event and publish video encoded event when encoding is done successfully.

--- video-service/src/main/java/com/netflix/videoservice/S3Config.java
+++
@@ -1,5 +0,0 @@
-package com.netflix.videoservice;
-
-public class S3Config {
-
-}
--- video-service/src/main/java/com/netflix/videoservice/config/KafkaConfig.java
+++ video-service/src/main/java/com/netflix/videoservice/config/KafkaConfig.java
@@ -1,5 +1,33 @@
 package com.netflix.videoservice.config;
 
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
+/**
+ * Setting kafka config
+ * 1. Adding new Topic when video is uploaded
+ * 2. Adding new Topic when video is encoding
+ * 
+ */
+
+@Configuration
 public class KafkaConfig {
+
+    @Bean
+    public NewTopic videoUploadedTopic(){
+        return TopicBuilder.name("video.uploaded")
+        .partitions(3)
+        .replicas(1)
+        .build();
+    }
+
+    @Bean
+    public NewTopic videoEncodedTopic(){
+        return TopicBuilder.name("video.encoded")
+        .partitions(3)
+        .replicas(1)
+        .build();
+    }
 
 }
